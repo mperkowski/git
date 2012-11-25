1220b66fe97f2dc4060f521b6d5e9de0786472d3
Maciej Perkowski <faint3k@gmail.com>
2012-11-25 11:39:49 +0100

    szachownica z gwiazdkami

---
+++ szach1.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int i,j,k;
+for (k=0; k<8; k++)
+{
+    for (i=0; i<8; i++)
+    {
+     printf("* ");
+    }
+
+printf("\n");
+               for (j=0; j<8; j++)
+               {
+               printf(" *");
+               }
+               printf("\n");
+                         
+}
+  	
+  return 0;
+}
