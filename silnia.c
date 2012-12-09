7f62e65c552b87974e2ba30e6810163a93b947f5
Maciej Perkowski <faint3k@gmail.com>
2012-12-09 12:58:26 +0100

    silnia

---
+++ silnia.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+  int a, i, silnia=1;
+  scanf("%d", &a);
+for(i=1; i<=a; i++)
+    {
+    silnia=(silnia*i);
+    }
+ printf ("%d\n", silnia);
+
+  return 0;
+}
+
+
+ 
